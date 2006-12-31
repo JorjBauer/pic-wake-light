0d21b98728a1a4280960956f6fa82493a3f364fc
jorj <jorj>
2006-12-31 20:09:34 +0000

    now has clock capability, but no alarm. The mode button works but not set.

--- globals.asm
+++ globals.asm
@@ -8,10 +8,19 @@
         GLOBAL  arg2
         GLOBAL  temp1
         GLOBAL  temp2
+
+	GLOBAL	time_current
 	GLOBAL	tens_hours
 	GLOBAL	ones_hours
 	GLOBAL	tens_minutes
 	GLOBAL	ones_minutes
+
+	GLOBAL	time_alarm
+	GLOBAL	tens_alarm_hours
+	GLOBAL	ones_alarm_hours
+	GLOBAL	tens_alarm_minutes
+	GLOBAL	ones_alarm_minutes
+	
 	GLOBAL	tens_seconds
 	GLOBAL	ones_seconds
 
@@ -24,10 +33,21 @@ temp1   res     1
 temp2   res     1
 ;;; ; ************************************************************************
 
+	;; the time data is accessed via INDF -- so order is important
+
+time_current	res	1
 tens_hours	res	1
 ones_hours	res	1
 tens_minutes	res	1
 ones_minutes	res	1
+
+time_alarm	res	1
+tens_alarm_hours	res	1
+ones_alarm_hours	res	1
+tens_alarm_minutes	res	1
+ones_alarm_minutes	res	1
+
+	;; yes, we need these AFTER the alarm time!
 tens_seconds	res	1
 ones_seconds	res	1
 
--- main.asm
+++ main.asm
@@ -14,6 +14,21 @@ _ResetVector	set	0x00
 _InitVector	set	0x04
 
 ;;; ************************************************************************
+;;; *
+;;; * TIMING ERROR NOTES
+;;; *
+;;; *   We're counting 59.779459-ish seconds per minute. That's 3586.77-ish
+;;; *   seconds per hour, so we need to make up 13-ish seconds an hour. or
+;;; *   we can count sub-seconds per minute, and set the prescalar lower...
+;;; *
+;;; *   ... but instead of that, we're just going to add 13 seconds to the
+;;; *   start of every hour. Which will leave us 5.57904 seconds short on a
+;;; *   day. So once a day, we'll add 5. We'll drift about 2 minutes a month.
+;;; *   Not fantastic, but tolerable.
+;;; ************************************************************************
+	
+
+;;; ************************************************************************
         udata
 
 save_w	res	1
@@ -24,6 +39,8 @@ tmrcnt		res	1	; timer counter
 	
 brightness	res	1	; 4-bit value for brightness of LED
 pulsate		res	1	; information on pulsate status
+
+mode		res	1	; current mode-button setting
 	
 ;;; ************************************************************************
         code
@@ -88,9 +105,7 @@ INT_TMR0:
 	;; because of the clock inaccuracy.
 	clrf	tmrcnt		; reset the timer count
 
-		
-	;; ***###*** FIXME: do something useful here
-	call pulsate_led
+	call	run_clock
 	goto	check_int	; be sure to loop in case of missed interrupt
 
 ;;; ************************************************************************
@@ -102,11 +117,6 @@ INT_TMR0:
 ;;; * Make the LED pulsate up/down with successive calls to pulsate_led.
 
 pulsate_led:
-	;; DEBUGGING: show the value on the 7-seg (FIXME)
-	movfw	brightness
-	call	display_digit
-	;; END DEBUGGING
-	
 	btfss	pulsate, 0	; if bit0 is set, we're pulsating down
 	goto	pulsate_up
 pulsate_down:
@@ -179,7 +189,84 @@ display_digit:
         bcf     STATUS, RP0     ; back to page 0
 	clrf	PORTB
 	return	
-		
+
+	
+;;; ************************************************************************
+;;; *
+;;; * add a second to the clock. If it's an hour, add 13 seconds more. If
+;;; * it's midnight, add an extra 5 seconds.
+;;; *
+;;; * if the alarm is going off, then figure out what to do!
+
+run_clock:
+	incf	ones_seconds, F
+	movfw	ones_seconds
+	xorlw	0x0A
+	skpz
+	return
+
+	;; ones-of-seconds rollover
+	clrf	ones_seconds
+	incf	tens_seconds, F
+	movfw	tens_seconds
+	xorlw	0x06
+	skpz
+	return
+
+	;; 60 seconds hit
+	clrf	tens_seconds
+	incf	ones_minutes, F
+	movfw	ones_minutes
+	xorlw	0x0A
+	skpz
+	goto	check_alarm_and_return
+
+	;; ones-of-minutes rollover
+	clrf	ones_minutes
+	incf	tens_minutes, F
+	movfw	tens_minutes
+	xorlw	0x06
+	skpz
+	goto	check_alarm_and_return
+
+	;; 60 minutes hit
+	movlw	0x03		; fudge the missing time-per-hour
+	movwf	ones_seconds
+	movlw	0x01
+	movwf	tens_seconds
+	clrf	tens_minutes
+	
+	incf	ones_hours, F
+	movfw	ones_hours
+	xorlw	0x0A
+	skpz
+	goto	check_for_midnight
+
+	;; ones-of-hours rollover
+	incf	tens_hours, F
+	goto	check_alarm_and_return ; day ends at 24 hours, not 10 or 20...
+
+check_for_midnight:
+	movfw	tens_hours
+	xorlw	0x02
+	skpz
+	goto	check_alarm_and_return
+	movfw	ones_hours
+	xorlw	0x04
+	skpz
+	goto	check_alarm_and_return
+
+	;; reached the end of the day (24:00:00, which is now 24:00:13).
+	clrf	ones_hours
+	clrf	tens_hours
+	movlw	0x05
+	addwf	ones_seconds, F	; add more fudge...
+	;; fall through
+
+check_alarm_and_return:	
+	;; FIXME: CHECK FOR THE ALARM CONDITION HERE!
+	return
+			
 ;;; ************************************************************************
 ;;; * Main
 ;;; *
@@ -200,8 +287,8 @@ Main:
 	bcf	PCON, OSCF	; set internal oscillator to 37kHz
 	bcf	OPTION_REG, PSA	; assign prescalar to TMR0
 	bcf	OPTION_REG, PS2	; set PS2..PS0 to 001 for 1:4; that means about
-	bsf	OPTION_REG, PS1	;   9 interrupts per second (9.033, give
-	bcf	OPTION_REG, PS0	;   take based on 37kHz clock).
+	bcf	OPTION_REG, PS1	;   9 interrupts per second (9.033, give
+	bsf	OPTION_REG, PS0	;   take based on 37kHz clock).
 	bcf	OPTION_REG, T0CS	; set TMR0 to timer mode
 
         bcf     STATUS, RP0     ; set up the page 0 registers
@@ -227,13 +314,73 @@ Main:
 	clrf	tmrcnt
 	clrf	brightness
 	clrf	pulsate
+	clrf	mode
 
+	clrf	tens_hours
+	clrf	ones_hours
+	clrf	tens_minutes
+	clrf	ones_minutes
+	clrf	tens_seconds
+	clrf	ones_seconds
+
+	clrf	tens_alarm_hours
+	movlw	0x06
+	movwf	ones_alarm_hours
+	clrf	tens_alarm_minutes
+	clrf	ones_alarm_minutes
+
+	movlw	0x0C		; 'C' for 'Current'
+	movwf	time_current
+	movlw	0x0A
+	movwf	time_alarm
+	
 main_loop:	
 	;; look for presses of either button. Delay, and do it again...
-	;; ***###*** FIXME:	 write this
+
+	;; The "mode" button is RA<4>, and the "set" button is RA<6>.
+	btfsc	PORTA, 4
+	call	mode_button
+	btfsc	PORTA, 6
+	call	set_button
+
+	movlw	0xFA		; delay 250mS
+	call	delay_ms
+	
 	goto main_loop
 
-	
+	;; MODES:[0] C HhMm A HhMm
+mode_button:
+	incf	mode, F
+	movfw	mode
+	xorlw	0x0B		; there are 10 modes. If we reach 11, turn off.
+	skpnz
+	clrf	mode		; set back to mode 0.
 
+	movfw	mode		; mode 0: disable TRISB and return
+	xorlw	0x00
+	skpnz
+	goto	disable_trisb
+
+	;; not mode 0. Figure out what to display and display it!
+	movlw	time_current - 1 ; start of our data block
+	addwf	mode, W		; add the current mode to it
+	movwf	FSR
+	movfw	INDF		; grab indirected pointer data
+
+	goto display_digit	; display that digit and return
+	
+set_button:	
+	return	
+
+disable_trisb:
+	movlw	0xFF		; set to all INPUTS
+        bcf     STATUS, RP1
+        bsf     STATUS, RP0     ; TRISB is in page 1
+	movwf	TRISB
+        bcf     STATUS, RP0     ; back to page 0
+	clrf	PORTB
+	return	
+
+	
 	END
 	
